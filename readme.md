a83913e70bedc7ce13c31cd853a5d3d01fd83f7b
achieveil <achieveil@outlook.com>
2023-12-23 16:01:15 +0800

    第一次提交

---
+++ readme.md
@@ -0,0 +1,46 @@
+# 基于KingST-51MCU的简单篮球比赛计时器
+
+## 前提
+
+VScode和[EIDE插件](https://marketplace.visualstudio.com/items?itemName=CL.eide),并且配置好工具链，配置[教程](https://zhuanlan.zhihu.com/p/669952673)
+
+## 适用
+
+金沙滩 51单片机
+![Alt text](Images/KingST-51.jpg)
+
+## 开始
+
+```powershell
+git clone https://github.com/achieveil/51MCU-BasketTimer.git
+cd 51MCU-BasketTimer
+code .
+```
+
+或者在下载使用vscode后打开`51MCU-BasketTimer.code-workspace`
+
+## 构建
+
+自定义代码后构建，生成hex文件，项目中已存在
+
+## 烧录到单片机
+
+使用EIDE的烧录命令（需要配置），或者使用其他烧录软件
+
+## 效果
+
+1. 系统上电瞬间，6位数码管的高2位显示计时剩余分钟数，中间2位显示计时剩余秒数，低2位显示24秒计时剩余。![Alt text](Images/run.jpg)
+2. 按键K1来进行计时开启/暂停，设置按键K2来进行24秒回表。
+3. 倒计时结束，LED小灯部分全亮，蜂鸣器蜂鸣，点阵全亮，示意比赛结束![Alt text](Images/alarm.jpg)
+
+模块连接
+![Alt text](images/image.png)
+按键控制模块
+设置按键K1来进行计时开启/暂停，设置按键K2来进行24秒回表。
+基本原理：4条输人线接到单片机的I/O口上，当按下按键K1时,+5V依次通过电阻R1和按键K1最终进入GND形成一条通路，这条线路的全部电压都加到电阻R1上,引脚KeyIn1就是一个低电平。当松开按键后,线路断开,不会有电流通过，KeyIn1和+5V应该是等电位，是一个高电平。因此，可以通过引脚KeyIn1这个I/O口的高低电平来判断是否有按键按下。
+
+## 本项目的初衷
+
+为有能力看到这的同学们尽微薄之力
+
+## end
---
+++ src/main.c
@@ -0,0 +1,370 @@
+#include <reg52.h>
+
+sbit ADDR0 = P1 ^ 0;
+sbit ADDR1 = P1 ^ 1;
+sbit ADDR2 = P1 ^ 2;
+sbit ADDR3 = P1 ^ 3;
+sbit ENLED = P1 ^ 4;
+
+sbit BUZZ = P1 ^ 6;
+
+sbit KEY1 = P2 ^ 4;
+sbit KEY2 = P2 ^ 5;
+sbit KEY3 = P2 ^ 6;
+sbit KEY4 = P2 ^ 7;
+
+
+/**
+ * @author achieveil,atnhaoyang
+ * @date 2023/12/22
+ */
+
+unsigned char code LedChar[] = { // 数码管显示字符转换表
+    0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,
+    0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E};
+unsigned char LedBuff[6] = { // 数码管显示缓冲区
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+unsigned char KeySta[4] = { // 按键当前状态
+    1, 1, 1, 1};
+
+bit RunFLag      = 0;  // 秒表运行标志
+bit Refresh      = 1;  // 秒表计数刷新标志
+signed char Low  = 24; // 低2位显示24秒计时剩余
+signed char Mid  = 0;  // 中间2位显示计时剩余秒数
+signed char High = 12; // 6位数码管的高2位显示计时剩余分钟数
+
+void Load();
+void KeyDriver();
+void all();
+void Reset();
+void StartBuzz();
+
+void main()
+{
+    EA    = 1; // 开总中断
+    ENLED = 0; // 使能选择数码管
+    ADDR3 = 1;
+    TMOD  = 0x01;
+    TH0   = 0xF8; // T0定时2ms
+    TL0   = 0xCD;
+    ET0   = 1;
+    TR0   = 1;
+
+    P2 = 0xF7; // P2.3置0，选择第1行按键作为独立按键
+
+    while (1) {
+        Load();
+        KeyDriver(); // 调用按键驱动函数
+    }
+}
+
+// 将数码管的值加载到缓冲区
+void Load()
+{
+    // 24部分加载到低2位
+    LedBuff[0] = LedChar[Low % 10];
+    LedBuff[1] = LedChar[Low / 10];
+    // 秒部分加载到中2位
+    LedBuff[2] = LedChar[Mid % 10];
+    LedBuff[3] = LedChar[Mid / 10];
+    // 分部分加载到高2位
+    LedBuff[4] = LedChar[High % 10];
+    LedBuff[5] = LedChar[High / 10];
+
+    LedBuff[2] &= 0x7F; // 点亮小数点
+}
+
+// 秒表复位函数
+void Reset()
+{
+    Low = 24;
+    Load(); // 确保暂停时可以讲24复位
+}
+
+// 秒表计数函数，每隔1s调用一次进行秒表计数累加
+void Count()
+{
+    unsigned int i = 0;
+    if (RunFLag) // 当处于运行状态时递增计数值
+    {
+        Low--;        // 24部分-1
+        if (Low <= 0) // 24部分为0
+        {
+            if (Mid >= 24 || High > 0)
+                Low = 24;
+            else if (Mid < 24)
+                Low = 0;
+        }
+        if (Mid <= 0) // 中间两位为0，重新计时，高两位减1
+        {
+            Mid = 60;
+            High--;
+        }
+        Mid--;
+        if (High == 0 && Mid == 0) // 计时结束，全部点亮
+        {
+            all();
+        }
+    }
+}
+
+// 按键驱动函数，检测按键动作
+void KeyDriver()
+{
+    unsigned char i;
+    static unsigned char backup[4] = {1, 1, 1, 1};
+
+    for (i = 0; i < 4; i++) // 循环检测4个按键
+    {
+        if (backup[i] != KeySta[i]) // 检测按键动作
+        {
+            if (backup[i] != 0) // 按键按下时执行动作
+            {
+                if (i == 1) // Esc键复位秒表
+                    Reset();
+                else if (i == 0) // 回车键启停秒表
+                    RunFLag = ~RunFLag;
+            }
+            backup[i] = KeySta[i]; // 刷新前一次的备份值
+        }
+    }
+}
+
+// 按键扫描函数
+void KeyScan()
+{
+    unsigned char i;
+    static unsigned char keybuf[4] = {// 按键扫描缓冲区
+                                      0xFF, 0xFF, 0xFF, 0xFF};
+
+    // 按键值移入缓冲区
+    keybuf[0] = (keybuf[0] << 1) | KEY1;
+    keybuf[1] = (keybuf[1] << 1) | KEY2;
+    keybuf[2] = (keybuf[2] << 1) | KEY3;
+    keybuf[3] = (keybuf[3] << 1) | KEY4;
+
+    // 消抖后更新按键状态
+    for (i = 0; i < 2; i++) {
+        if (keybuf[i] == 0x00) { // 连续8次扫描值为0，即16ms内都是按下状态时，可认为按键已稳定的按下
+            KeySta[i] = 0;
+        } else if (keybuf[i] == 0xFF) { // 连续8次扫描值为1，即16ms内都是弹起状态时，可认为按键已稳定的弹起
+            KeySta[i] = 1;
+        }
+    }
+}
+
+// 数码管动态扫描刷新函数，需在定时中断中调用
+void LedScan()
+{
+    static unsigned char i = 0; // 动态扫描索引
+
+    P0 = 0xFF; // 关闭所有段选位，显示消隐
+    switch (i) {
+        case 0:
+            ADDR2 = 0;
+            ADDR1 = 0;
+            ADDR0 = 0;
+            i++;
+            P0 = LedBuff[0];
+            break;
+        case 1:
+            ADDR2 = 0;
+            ADDR1 = 0;
+            ADDR0 = 1;
+            i++;
+            P0 = LedBuff[1];
+            break;
+        case 2:
+            ADDR2 = 0;
+            ADDR1 = 1;
+            ADDR0 = 0;
+            i++;
+            P0 = LedBuff[2];
+            break;
+        case 3:
+            ADDR2 = 0;
+            ADDR1 = 1;
+            ADDR0 = 1;
+            i++;
+            P0 = LedBuff[3];
+            break;
+        case 4:
+            ADDR2 = 1;
+            ADDR1 = 0;
+            ADDR0 = 0;
+            i++;
+            P0 = LedBuff[4];
+            break;
+        case 5:
+            ADDR2 = 1;
+            ADDR1 = 0;
+            ADDR0 = 1;
+            i     = 0;
+            P0    = LedBuff[5];
+            break;
+        default:
+            break;
+    }
+}
+void all()
+{
+    unsigned char x = 0;
+    unsigned char i = 0;
+    while (1) {
+        P0 = 0xFF;
+        switch (x) {
+            // 点亮数码管
+            case 0:
+                ADDR3 = 1;
+                ADDR2 = 0;
+                ADDR1 = 0;
+                ADDR0 = 0;
+                x++;
+                P0 = LedChar[0];
+                break;
+            case 1:
+                ADDR3 = 1;
+                ADDR2 = 0;
+                ADDR1 = 0;
+                ADDR0 = 1;
+                x++;
+                P0 = LedChar[0];
+                break;
+            case 2:
+                ADDR3 = 1;
+                ADDR2 = 0;
+                ADDR1 = 1;
+                ADDR0 = 0;
+                x++;
+                P0 = LedChar[0];
+                break;
+            case 3:
+                ADDR3 = 1;
+                ADDR2 = 0;
+                ADDR1 = 1;
+                ADDR0 = 1;
+                x++;
+                P0 = LedChar[0];
+                break;
+            case 4:
+                ADDR3 = 1;
+                ADDR2 = 1;
+                ADDR1 = 0;
+                ADDR0 = 0;
+                x++;
+                P0 = LedChar[0];
+                break;
+            case 5:
+                ADDR3 = 1;
+                ADDR2 = 1;
+                ADDR1 = 0;
+                ADDR0 = 1;
+                x++;
+                P0 = LedChar[0];
+                break;
+
+            // 点亮小灯
+            case 6:
+                ADDR3 = 1;
+                ADDR2 = 1;
+                ADDR1 = 1;
+                ADDR0 = 0;
+                x++;
+                P0 = 0x00;
+                break;
+
+            // 点亮点阵
+            case 7:
+                ADDR3 = 0;
+                ADDR2 = 0;
+                ADDR1 = 0;
+                ADDR0 = 0;
+                x++;
+                P0 = 0x00;
+                break;
+            case 8:
+                ADDR3 = 0;
+                ADDR2 = 0;
+                ADDR1 = 0;
+                ADDR0 = 1;
+                x++;
+                P0 = 0x00;
+                break;
+            case 9:
+                ADDR3 = 0;
+                ADDR2 = 0;
+                ADDR1 = 1;
+                ADDR0 = 0;
+                x++;
+                P0 = 0x00;
+                break;
+            case 10:
+                ADDR3 = 0;
+                ADDR2 = 0;
+                ADDR1 = 1;
+                ADDR0 = 1;
+                x++;
+                P0 = 0x00;
+                break;
+            case 11:
+                ADDR3 = 0;
+                ADDR2 = 1;
+                ADDR1 = 0;
+                ADDR0 = 0;
+                x++;
+                P0 = 0x00;
+                break;
+            case 12:
+                ADDR3 = 0;
+                ADDR2 = 1;
+                ADDR1 = 0;
+                ADDR0 = 1;
+                x++;
+                P0 = 0x00;
+                break;
+            case 13:
+                ADDR3 = 0;
+                ADDR2 = 1;
+                ADDR1 = 1;
+                ADDR0 = 0;
+                x++;
+                P0 = 0x00;
+                break;
+            case 14:
+                ADDR3 = 0;
+                ADDR2 = 1;
+                ADDR1 = 1;
+                ADDR0 = 1;
+                x     = 0;
+                P0    = 0x00;
+                break;
+            default:
+                break;
+        }
+        for (i = 0; i <= 200; i++)
+            ;
+        StartBuzz();
+    }
+}
+
+void StartBuzz()
+{
+    BUZZ = ~BUZZ;
+}
+
+// T0中断服务函数，完成数码管、按键扫描与秒表计数
+void InterruptTimer0() interrupt 1
+{
+    static unsigned int tmr2ms = 0;
+
+    TH0 = 0xF8; // 重新加载重载值
+    TL0 = 0xCD;
+    LedScan(); // 数码管扫描显示
+    KeyScan(); // 按键扫描
+    // 定时10ms进行一次秒表计数
+    tmr2ms++;
+    if (tmr2ms >= 500) {
+        tmr2ms = 0;
+        Count(); // 调用秒表计数函数
+    }
+}
